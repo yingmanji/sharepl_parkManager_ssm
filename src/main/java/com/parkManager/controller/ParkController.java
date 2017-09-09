34ebbcfda38f8c48f899d15aeea402f226bfa540
yingmanji <2283438112@qq.com>
2017-09-10 00:30:48 +0800

    controller层

---
+++ src/main/java/com/parkManager/controller/ParkController.java
@@ -0,0 +1,37 @@
+package com.parkManager.controller;
+
+import com.parkManager.entitiy.ParkinglotCustom;
+import com.parkManager.service.UserParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by 樱满集0_0 on 2017/9/9.
+ */
+
+@Controller
+public class ParkController{
+
+    @Autowired
+    private UserParkingService userParkingService;
+
+    @RequestMapping(value = "queryUserParkingById.action",method = {RequestMethod.GET,RequestMethod.POST})
+    public String queryUserParkingById(Model model, HttpServletRequest request, Integer id)throws Exception{
+
+        List<ParkinglotCustom> parkinglotCustoms=userParkingService.selectUsersParkinglot(id);
+
+        model.addAttribute("ParkinglotCustoms",parkinglotCustoms);
+
+        return "parks/parksList";
+
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/parks/parksList.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 樱满集0_0
+  Date: 2017/9/10
+  Time: 0:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form>
+        <table>
+            停车场管理员数据:
+            <tr>
+                <td>租车者姓名</td>
+                <td>租车者手机号</td>
+                <td>所租车位地址</td>
+                <td>所属停车场id</td>
+                <td>车位所有者姓名</td>
+                <td>车位所有者手机号</td>
+            </tr>
+
+            <tr>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
